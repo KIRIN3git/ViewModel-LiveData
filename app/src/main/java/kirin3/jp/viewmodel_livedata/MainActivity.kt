828d80fe28fc2646548f1ca879808dc2d0555a29
KIRIN3 <scomputer13@gmail.com>
2019-08-17 19:37:53 +0900

    調整

--- app/src/main/java/kirin3/jp/viewmodel_livedata/MainActivity.kt
+++ app/src/main/java/kirin3/jp/viewmodel_livedata/MainActivity.kt
@@ -4,7 +4,6 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -17,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         // アクティビティに属するカウンター
         var counterA = 0
 
-        // 何度も使いまわされるインスタンスを作成
+        // インスタンスを作成
         val viewModel: CountViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
 
         val text1 = findViewById(R.id.text1) as TextView
@@ -29,18 +28,17 @@ class MainActivity : AppCompatActivity() {
             text1.setText(counterA.toString())
 
             // ViewModelの監視対象のメンバ変数を+1
+            // 値を取り出す場合は[value]
             viewModel.counterB.value = viewModel.counterB.value!! + 1
         }
 
-        // UI更新用のオブザーバーを作成
+        // UI更新用のオブザーバー（監視役）を作成
         val countObserver = Observer<Int> { counter ->
-            // TextViewを更新
+            // 更新を受け取ったらTextViewを更新
             text2.text = counter.toString()
         }
 
         // LiveDataを観察し、このアクティビティをLifecycleOwnerおよびオブザーバーとして渡す
         viewModel.counterB.observe(this, countObserver)
-
-
     }
 }
